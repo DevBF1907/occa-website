89146a6cc2d42b3229cb469da3cfa4fd5d83272c
Brenno Felipe <devbrenno1907@gmail.com>
2026-06-25 21:06:23 -0300

    chore: remove .next/ do tracking, adiciona logo OCCA e export out/

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ePKa+jMYpjfQ4PEIxHzg/ecWwS9png4h+ovFF2sWjtI=\"\n}"
--- .next/dev/static/chunks/src_app_services_cooccaspace_page_tsx_0-0lnb2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0co606e._.js","static/chunks/node_modules_04fg904._.js"],
-    source: "dynamic"
-});
